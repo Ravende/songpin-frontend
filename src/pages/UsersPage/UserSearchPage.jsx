df575b4462b38e7026487363332614fbbdcc3512
yoonjin-C <cyy000123@gmail.com>
2024-07-03 10:21:26 +0200

    💄 UI : 유저 검색 페이지 퍼블리싱

---
+++ src/pages/UsersPage/UserSearchPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const UserSearchPage = () => {
+  return <UserSearchContainer></UserSearchContainer>;
+};
+
+export default UserSearchPage;
+
+const UserSearchContainer = styled.div``;
+
+const Content = styled.div`
+  margin: 33px;
+  padding-top: 40px;
+  display: flex;
+  flex-direction: column;
+`;
