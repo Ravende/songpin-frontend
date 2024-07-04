1eb07a1cc69e36f26e0ea30c79557b40ce86fe96
Gabin Lee <gb94@ewhain.net>
2024-07-04 17:07:41 +0900

    💄 UI: 사이드바 퍼블리싱

--- src/components/HomePage/SideBar.jsx
+++ src/components/HomePage/SideBar.jsx
@@ -1,23 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import sidebar_logo from '../../assets/sidebar/sidebar_logo.svg';
+import sidebar_home from '../../assets/sidebar/sidebar_home.svg';
+import sidebar_search from '../../assets/sidebar/sidebar_search.svg';
+import sidebar_create from '../../assets/sidebar/sidebar_create.svg';
+import sidebar_playlist from '../../assets/sidebar/sidebar_playlist.svg';
+import sidebar_usersearch from '../../assets/sidebar/sidebar_usersearch.svg';
+import sidebar_mypage from '../../assets/sidebar/sidebar_mypage.svg';
 
 const SideBar = () => {
+    const navigate = useNavigate();
+
+    const onItemClick = (path) => {
+        navigate(path);
+    };
+
     return (
         <SideBarContainer>
             <PinFeatures>
-                <ul>
-                    <li><button onClick={() => onItemClick('Home Logo')}>Home</button></li>
-                    <li><button onClick={() => onItemClick('Home')}>Home</button></li>
-                    <li><button onClick={() => onItemClick('Search Pin')}>Search</button></li>
-                    <li><button onClick={() => onItemClick('Create Pin')}>Create</button></li>
-                    <li><button onClick={() => onItemClick('Playlist')}>Playlist</button></li>
-                </ul>
+                <StyledButton onClick={() => onItemClick('/home')} noBackground>
+                    <HomeLogo src={sidebar_logo} alt="Home Logo" />
+                </StyledButton>
+                <StyledButton onClick={() => onItemClick('/home')}>
+                    <Home src={sidebar_home} alt="Home" />
+                </StyledButton>
+                <StyledButton onClick={() => onItemClick('/search')}>
+                    <Search src={sidebar_search} alt="Search" />
+                </StyledButton>
+                <StyledButton onClick={() => onItemClick('/create')}>
+                    <Create src={sidebar_create} alt="Create" />
+                </StyledButton>
+                <StyledButton onClick={() => onItemClick('/playlist')}>
+                    <Playlist src={sidebar_playlist} alt="Playlist" />
+                </StyledButton>
             </PinFeatures>
             <UserFeatures>
-                <ul>
-                    <li><button onClick={() => onItemClick('Search User')}>Search User</button></li>
-                    <li><button onClick={() => onItemClick('MyPage')}>MyPage</button></li>
-                </ul>
+                <StyledButton onClick={() => onItemClick('/users')}>
+                    <UserSearch src={sidebar_usersearch} alt="Search User" />
+                </StyledButton>
+                <StyledButton onClick={() => onItemClick('/mypage')}>
+                    <MyPage src={sidebar_mypage} alt="MyPage" />
+                </StyledButton>
             </UserFeatures>
         </SideBarContainer>
     );
@@ -25,12 +49,128 @@ const SideBar = () => {
 
 const SideBarContainer = styled.div`
     position: fixed;
-    top: 0;
-    left: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
     width: 80px;
     height: 100%;
-    background-color: white;
-    padding: 20px;
+    border: 1px solid var(--gray, #BCBCBC);
+    background: var(--f8f8f8, #FCFCFC);
+`;
+
+const PinFeatures = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    margin-top: 20px;
+    margin-bottom: 20px;
+`;
+
+const UserFeatures = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
+    width: 100%;
+    margin-top: 50px;
+    margin-bottom: 50px;
+`;
+
+const StyledButton = styled.button`
+    background: none;
+    border: none;
+    padding: 0;
+    margin: 20px 0;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:focus {
+        outline: none;
+    }
+
+    &:focus::after {
+        content: '';
+        position: absolute;
+        width: 60px;
+        height: 60px;
+        background: ${({ noBackground }) => (noBackground ? 'none' : 'var(--light_black, #232323)')};
+        border-radius: 50%;
+        z-index: -1;
+    }
+
+    &:hover {
+    }
+`;
+
+const HomeLogo = styled.img`
+    width: 40px;
+    height: 40px;
+`;
+
+const Home = styled.img`
+    width: 40px;
+    height: 40px;
+    transition: filter 0.3s ease;
+
+    ${StyledButton}:focus & {
+        filter: brightness(0) invert(1);
+    }
+`;
+
+const Search = styled.img`
+    width: 27.308px;
+    height: 27.308px;
+    padding: 16px;
+    transition: filter 0.3s ease;
+
+    ${StyledButton}:focus & {
+        filter: brightness(0) invert(1);
+    }
+`;
+
+const Create = styled.img`
+    width: 40px;
+    height: 40px;
+    transition: filter 0.3s ease;
+
+    ${StyledButton}:focus & {
+        filter: brightness(0) invert(1);
+    }
+`;
+
+const Playlist = styled.img`
+    width: 43px;
+    height: 43px;
+    transition: filter 0.3s ease;
+
+    ${StyledButton}:focus & {
+        filter: brightness(0) invert(1);
+    }
+`;
+
+const UserSearch = styled.img`
+    width: 40px;
+    height: 40px;
+    transition: filter 0.3s ease;
+
+    ${StyledButton}:focus & {
+        filter: brightness(0) invert(1);
+    }
+`;
+
+const MyPage = styled.img`
+    width: 30px;
+    height: 28px;
+    transition: filter 0.3s ease;
+
+    ${StyledButton}:focus & {
+        filter: brightness(0) invert(1);
+    }
 `;
 
 export default SideBar;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,9 @@
-const HomePage = () => {};
+import SideBar from '../../components/HomePage/SideBar';
+
+const HomePage = () => {
+    return(
+        <SideBar />
+    )
+};
 
 export default HomePage;
