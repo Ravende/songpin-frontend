c096da4ad6a35e891cf2074f16136157291722bb
yoonjin-C <cyy000123@gmail.com>
2024-07-08 13:18:05 +0200

    💄 UI : 플레이리스트 메인 페이지 퍼블리싱

---
+++ src/components/PlaylistPage/Playlist.jsx
@@ -0,0 +1,200 @@
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import nobookmark from '../../assets/images/PlaylistPage/bookmark-no.svg';
+import yesbookmark from '../../assets/images/PlaylistPage/bookmark-yes.svg';
+import pinImage from '../../assets/images/MusicSearchPage/spark_122.svg';
+
+const Playlist = (/* { playlist }*/) => {
+  //   const { isBookmarked, coverImages } = playlist;
+  const [isHovered, setIsHovered] = useState(false);
+  const [isBookmarked, setIsBookmarked] = useState(false); // 예시로 초기값을 false로 설정
+
+  const toggleBookmark = () => {
+    setIsBookmarked((prev) => !prev); // 상태 반전
+  };
+  return (
+    <PlaylistContainer>
+      <PlaylistBox>
+        <BigBox>
+          {/* 북마크 버튼 클릭하면 바뀌어야 함 */}
+          <BookmarkBtn src={isBookmarked ? yesbookmark : nobookmark} alt="북마크 버튼" onClick={toggleBookmark} />
+        </BigBox>
+        <SmallBoxContainer>
+          <SmallBox />
+          <SmallBox />
+          {/* <SmallBox imageUrl={coverImages[1]} />
+          <SmallBox imageUrl={coverImages[2]} /> */}
+        </SmallBoxContainer>
+      </PlaylistBox>
+      <PlaylistNameContainer onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+        <PlaylistName isHovered={isHovered}>
+          {/*{playlistName}*/}&nbsp;가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타
+          &nbsp;가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타
+        </PlaylistName>
+        <FadeOut />
+      </PlaylistNameContainer>
+      <NameBox>
+        <UserName>by 송송</UserName>
+        <PinBox>
+          <PinImg src={pinImage} alt="핀이미지" />
+          <PinNum>53</PinNum>
+        </PinBox>
+      </NameBox>
+      <UpdatedDate>20xx.xx.xx updated</UpdatedDate>
+    </PlaylistContainer>
+  );
+};
+
+export default Playlist;
+
+const PlaylistContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 15px;
+`;
+const PlaylistBox = styled.div`
+  display: flex;
+  overflow: hidden;
+  margin-bottom: 8px;
+`;
+
+const BigBox = styled.div`
+  width: 140px;
+  height: 140px;
+  border-radius: 8px 0px 0px 8px;
+  border: 1px solid var(--f8f8f8, #fcfcfc);
+  background: #5452ff;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+
+const SmallBoxContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+// const SmallBox = styled.div`
+//   width: 70px;
+//   height: 70px;
+//   border: 1px solid var(--f8f8f8, #fcfcfc);
+//   background: url(${props => props.imageUrl}) no-repeat center center;
+//   background-size: cover;
+//   &:first-child {
+//     border-radius: 0px 8px 0px 0px;
+//   }
+//   &:last-child {
+//     border-radius: 0px 0px 8px 0px;
+//   }
+// `;
+
+const SmallBox = styled.div`
+  width: 70px;
+  height: 70px;
+  border: 1px solid var(--f8f8f8, #fcfcfc);
+  &:first-child {
+    border-radius: 0px 8px 0px 0px;
+    background: #00d2d2;
+  }
+  &:last-child {
+    border-radius: 0px 0px 8px 0px;
+    background: var(--offwhite, #efefef);
+  }
+`;
+
+const BookmarkBtn = styled.img`
+  width: 30px;
+  height: 30px;
+  flex-shrink: 0;
+  padding: 10px;
+  cursor: pointer;
+`;
+
+const PlaylistNameContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  width: 210px;
+  overflow: hidden;
+`;
+
+const scrollText = keyframes`
+  0% {
+    transform: translateX(0%);
+  }
+  100% {
+    transform: translateX(-50%);
+  }
+`;
+
+const PlaylistName = styled.div`
+  color: var(--light_black, #232323);
+  font-family: Pretendard;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  padding: 6px 6px 6px 2px;
+  white-space: nowrap;
+  animation: ${(props) => (props.isHovered ? scrollText : 'none')} 9s linear infinite;
+`;
+
+const FadeOut = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  width: 40px;
+  background: linear-gradient(to left, white, rgba(255, 255, 255, 0));
+`;
+
+const NameBox = styled.div`
+  width: 210px;
+  height: 24px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const UserName = styled.div`
+  color: var(--gray03, #5f5f5f);
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 24px */
+  margin-left: 8px;
+`;
+
+const PinBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const PinImg = styled.img`
+  width: 15px;
+  height: 15px;
+  opacity: 0.8;
+`;
+
+const PinNum = styled.div`
+  color: var(--light_black, #232323);
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 140%; /* 22.4px */
+  padding: 5px;
+`;
+
+const UpdatedDate = styled.div`
+  color: var(--gray02, #747474);
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 24px */
+  height: 24px;
+  padding: 8px;
+`;
--- src/components/PlaylistPage/PlaylistSideSection.jsx
+++ src/components/PlaylistPage/PlaylistSideSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import SearchBar from '../UsersPage/SearchBar';
+import Playlist from './Playlist';
 
 const PlaylistSideSection = () => {
   return (
@@ -10,8 +11,14 @@ const PlaylistSideSection = () => {
         <ContentBox>
           <SearchBar placeholder="플레이리스트 검색" />
         </ContentBox>
-        {/* <h2>최근 생성된 플레이리스트</h2>
-        <h2>팔로잉 중인 사람들의 플레이리스트</h2> */}
+        <TitleText>최근 생성된 플레이리스트</TitleText>
+        <PlaylistFeed>
+          <Playlist />
+          <Playlist />
+          <Playlist />
+        </PlaylistFeed>
+        <TitleText>팔로잉 중인 사람들의 플레이리스트</TitleText>
+        <NoPlaylist>표시할 플레이리스트가 없습니다.</NoPlaylist>
       </SideBox>
     </SideComponent>
   );
@@ -35,9 +42,47 @@ const SideBox = styled.div`
   width: 528px;
   border-right: 1px solid var(--gray, #bcbcbc);
   /* padding: 33px; */
+  /* 스크롤바 왜 보이는지???
+  overflow-y: auto;
+  overflow-x: hidden;
+  ::-webkit-scrollbar {
+    display: none;
+  } */
 `;
 
 const ContentBox = styled.div`
   padding: 33px;
   padding-top: 40px;
+  padding-bottom: 15px;
+`;
+
+const TitleText = styled.div`
+  color: var(--light_black, #232323);
+  font-family: Pretendard;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 40px; /* 125% */
+  padding-left: 35px;
+  margin-bottom: 15px;
+`;
+
+const PlaylistFeed = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin: 0px 20px 70px 20px;
+`;
+
+const NoPlaylist = styled.div`
+  color: var(--gray02, #747474);
+  width: 528px;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 140%; /* 28px */
+  display: flex;
+  justify-content: center;
+  padding-top: 130px;
+  padding-bottom: 130px;
 `;
