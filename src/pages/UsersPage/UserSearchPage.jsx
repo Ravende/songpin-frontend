ba13a03860714341c2e7c0b49aa02479a76aa373
yoonjin-C <cyy000123@gmail.com>
2024-07-18 21:52:05 +0200

    💄 UI : 타 유저 플레이리스트 피드 UI 수정

--- src/pages/UsersPage/UserSearchPage.jsx
+++ src/pages/UsersPage/UserSearchPage.jsx
@@ -9,9 +9,9 @@ import UserInfo from '../../components/UsersPage/UserInfo';
 const UserSearchPage = () => {
   return (
     <SideSection>
-        <ContentBox>
+        <SearchBox>
           <SearchBar placeholder="핸들 또는 닉네임을 검색" />
-        </ContentBox>
+        </SearchBox>
         <ContentBox>
           <UserInfo />
         </ContentBox>
@@ -27,4 +27,14 @@ const ContentBox = styled.div`
   border-bottom: 1px solid var(--gray, #bcbcbc);
 `;
 
+const SearchBox = styled.div`
+  padding: 40px 34px 10px 34px;
+  border-bottom: 1px solid var(--gray, #bcbcbc);
+`
+
+const Line = styled.div`
+  width: 528px;
+  height: 1px;
+  background: var(--gray, #bcbcbc);
+`;
 
