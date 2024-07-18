e4f0979d4e1d99fcb3d49beef566d2beff350365
Minyong Kim <ravenade88@gmail.com>
2024-07-18 21:26:57 +0900

    💄 UI: 핀 메모 컴포넌트 더보기 숨김 처리 진행 상황 저장

--- src/components/MusicSearchPage/MusicInfoPinPreview.jsx
+++ src/components/MusicSearchPage/MusicInfoPinPreview.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import lockIcon from '../../assets/images/MyPage/lock.svg';
+import { useNavigate } from 'react-router-dom';
 
 const MusicInfoPinPreview = () => {
+  const [isTruncated, setIsTruncated] = useState(true);
+
+  const toggleTruncation = () => {
+    setIsTruncated(!isTruncated);
+  };
+
+  const text =
+    '사랑하긴 했었나요 스쳐가는 인연이었나요 누가 내 가슴에다 불을 질렀나 누가 내 심장에다 못을 박았나 그대의 눈빛은 날 얼어붙게 해어쩌구';
+
+  const displayText = isTruncated ? text.substring(0, 68) : text;
+
+  const navigate = useNavigate();
+  const goUsersPage = () => {
+    navigate('/users');
+  };
+
   return (
     <PinsContainer>
-      <PinPreview>
+      <PinPreview isTruncated={isTruncated}>
         <PinContent>
-          <UserName>채연</UserName>
-          <PinMemo>
+          <UserName onClick={goUsersPage}>채연</UserName>
+          <PinMemo isTruncated={isTruncated}>
             <SecretPin src={lockIcon} />
-            사랑하긴 했었나요 스쳐가는 인연이었나요 누가 내 가슴에다 불을 질렀나 누가 내 심장에다 못을 박았나 그대의
-            눈빛은 날 얼어붙게 해 그대의
-            <MoreBtn>...더보기</MoreBtn>
+            {displayText}
+            {isTruncated && <MoreBtn onClick={toggleTruncation}> ...더보기</MoreBtn>}
           </PinMemo>
           <Details>
             <Date>2024.04.04</Date>
@@ -33,7 +49,8 @@ const PinsContainer = styled.div`
 
 const PinPreview = styled.div`
   width: 462px;
-  max-height: 137px;
+  /* max-height: 137px; */
+  /* max-height: ${(props) => (props.isTruncated ? '137px' : 'auto')}; */
   min-height: 113px;
   flex-shrink: 0;
   border-radius: 8px;
@@ -45,6 +62,7 @@ const PinContent = styled.div`
   display: flex;
   flex-direction: column;
   padding: 17px 21px 15px 15px;
+  display: inline-block;
 `;
 
 const UserName = styled.div`
@@ -54,35 +72,37 @@ const UserName = styled.div`
   font-style: normal;
   font-weight: 400;
   line-height: 140%; /* 22.4px */
-  padding-left: 3px;
+  cursor: pointer;
+  display: inline-block;
 `;
 
 const PinMemo = styled(UserName)`
   line-height: 150%; /* 24px */
   padding-top: 7px;
-  overflow: hidden;
-  /* text-overflow: ellipsis; */
-  display: -webkit-box;
+  cursor: auto;
+  /* display: -webkit-box;
   -webkit-box-orient: vertical;
-  -webkit-line-clamp: 2;
+  -webkit-line-clamp: 2; */
+  /* max-height: ${(props) => (props.isTruncated ? '48px' : 'none')}; 2줄 이상 넘어가면 끊기 */
 `;
 
 const SecretPin = styled.img`
   width: 13px;
   height: 16px;
   flex-shrink: 0;
-  padding-right: 7px;
-  padding-left: 1px;
+  padding-right: 8px;
+  padding-left: 3px;
   vertical-align: calc(-10%);
 `;
 
-const MoreBtn = styled.div`
+const MoreBtn = styled.span`
   color: var(--gray02, #747474);
   font-family: Pretendard;
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
   line-height: 150%;
+  cursor: pointer;
 `;
 
 const Details = styled.div`
