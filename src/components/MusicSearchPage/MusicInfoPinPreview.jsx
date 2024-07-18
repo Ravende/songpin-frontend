d71bcc63a510f3d90fbe0508dfe4ffc44d476ab9
Minyong Kim <ravenade88@gmail.com>
2024-07-18 22:32:02 +0900

    💄 UI: 핀 메모 더보기 수정

--- src/components/MusicSearchPage/MusicInfoPinPreview.jsx
+++ src/components/MusicSearchPage/MusicInfoPinPreview.jsx
@@ -11,9 +11,9 @@ const MusicInfoPinPreview = () => {
   };
 
   const text =
-    '사랑하긴 했었나요 스쳐가는 인연이었나요 누가 내 가슴에다 불을 질렀나 누가 내 심장에다 못을 박았나 그대의 눈빛은 날 얼어붙게 해어쩌구';
+    '사랑하긴 했었나요 스쳐가는 인연이었나요 누가 내 가슴에다 불을 질렀나 누가 내 심장에다 못을 박았나 그대의 눈빛은 날 얼어붙게 해 어쩌구';
 
-  const displayText = isTruncated ? text.substring(0, 68) : text;
+  const displayText = isTruncated ? text.substring(0, 55) : text;
 
   const navigate = useNavigate();
   const goUsersPage = () => {
@@ -22,10 +22,10 @@ const MusicInfoPinPreview = () => {
 
   return (
     <PinsContainer>
-      <PinPreview isTruncated={isTruncated}>
+      <PinPreview>
         <PinContent>
           <UserName onClick={goUsersPage}>채연</UserName>
-          <PinMemo isTruncated={isTruncated}>
+          <PinMemo onClick={isTruncated ? () => {} : toggleTruncation} isTruncated={isTruncated}>
             <SecretPin src={lockIcon} />
             {displayText}
             {isTruncated && <MoreBtn onClick={toggleTruncation}> ...더보기</MoreBtn>}
@@ -49,8 +49,6 @@ const PinsContainer = styled.div`
 
 const PinPreview = styled.div`
   width: 462px;
-  /* max-height: 137px; */
-  /* max-height: ${(props) => (props.isTruncated ? '137px' : 'auto')}; */
   min-height: 113px;
   flex-shrink: 0;
   border-radius: 8px;
@@ -79,11 +77,7 @@ const UserName = styled.div`
 const PinMemo = styled(UserName)`
   line-height: 150%; /* 24px */
   padding-top: 7px;
-  cursor: auto;
-  /* display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 2; */
-  /* max-height: ${(props) => (props.isTruncated ? '48px' : 'none')}; 2줄 이상 넘어가면 끊기 */
+  cursor: ${(props) => (props.isTruncated ? 'auto' : 'pointer')};
 `;
 
 const SecretPin = styled.img`
