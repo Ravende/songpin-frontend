ee45bb8a6d1b6d792eca65b175ca923139464f44
yoonjin-C <cyy000123@gmail.com>
2024-07-19 01:08:38 +0200

    💄 UI : 플레이리스트 편집 페이지 퍼블리싱

--- src/pages/PlaylistPage/PlaylistEditPage.jsx
+++ src/pages/PlaylistPage/PlaylistEditPage.jsx
@@ -4,32 +4,58 @@ import { useNavigate } from 'react-router-dom';
 import backArrow from '../../assets/images/UsersPage/arrow_back_ios.svg';
 import checked from '../../assets/images/PlaylistPage/checkbox_checked.svg';
 import unchecked from '../../assets/images/PlaylistPage/checkbox_unchecked.svg';
+import ToggleOn from '../../assets/images/PlaylistPage/toggle_on.svg'
 import PinComponent from '../../components/PlaylistPage/PinComponent';
-import PlaylistEditSideSection from '../../components/PlaylistPage/PlaylistEditSideSection';
 import SideSection from '../../components/common/SideSection';
 
 const PlaylistEditPage = () => {
   const navigate = useNavigate();
   const [isChecked, setIsChecked] = useState(false);
+  const [isPublic, setIsPublic] = useState(true); // 추가: 공개 여부 상태
+  const [inputValue, setInputValue] = useState('페스티벌에서 듣고싶은 노래');
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    if (value.length <= 40) {
+      setInputValue(value);
+    }
+  };
+
   const handleBackClick = () => {
     navigate(-1);
   };
   const handleCheckClicked = () => {
     setIsChecked(!isChecked);
   };
+
+  const handleToggleClick = () => {
+    setIsPublic((prev) => !prev);
+  };
+
   return (
     <SideSection>
       <EditContainer>
         <ContentBox>
           <BackBtn src={backArrow} onClick={handleBackClick} />
           <MainText>플레이리스트 편집</MainText>
-          <BtnText>완료</BtnText>
+          <BtnText isDisabled={!inputValue} onClick={handleBackClick}>완료</BtnText>
         </ContentBox>
         <ContentBox>
           <BodyText>플레이리스트 이름</BodyText>
+          <Edit>
+              <EditBox>              
+                <EditText type="text" value={inputValue} onChange={handleChange} /> 
+              <AlarmMessage>{inputValue.length}/40</AlarmMessage>
+              </EditBox>
+              <Line />
+            </Edit>
         </ContentBox>
         <ContentBox>
           <BodyText>공개 여부</BodyText>
+          <ToggleBox >
+            <ToggleText>{isPublic ? '공개' : '비공개'}</ToggleText>
+            <ToggleBtn src={ToggleOn}   isPublic={isPublic} onClick={handleToggleClick}/>
+          </ToggleBox>
         </ContentBox>
         <ContentBox>
           <SelectBox>
@@ -82,7 +108,7 @@ const MainText = styled.div`
 `;
 
 const BtnText = styled.div`
-  color: var(--light_black, #232323);
+    color: ${({ isDisabled }) => (isDisabled ? 'var(--gray, #BCBCBC)' : 'var(--light_black, #232323)')};
   font-family: Pretendard;
   font-size: 20px;
   font-style: normal;
@@ -124,3 +150,65 @@ const SelectBox = styled.div`
 `;
 
 const PinContainer = styled.div``;
+
+const Edit = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const EditText = styled.input`
+  color: var(--light_black, #232323);
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 140%; /* 28px */
+  border: none;
+  outline: none;
+`;
+
+const Line = styled.div`
+  width: 293px;
+  height: 1px;
+  background: var(--gray02, #747474);
+`;
+
+const AlarmMessage = styled.div`
+  color: var(--gray02, #747474);
+  text-align: right;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 24px */
+`;
+
+const EditBox = styled.div`
+display:flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+`
+
+const ToggleBox = styled.div`
+display:flex;
+flex-direction: row;
+align-items: center;
+`
+
+const ToggleText = styled.div`
+color: var(--gray02, #747474);
+font-family: Pretendard;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: 150%; /* 24px */
+padding-right:16px;
+`
+
+const ToggleBtn = styled.img`
+width: 60px;
+height: 30px;
+ transform: ${({ isPublic }) => (isPublic ? 'rotate(0deg)' : 'rotate(180deg)')};
+ cursor:pointer;
+`
