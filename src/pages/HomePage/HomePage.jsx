ccbd69c33edaf2beeab0a044f33bf13d227b9052
Gabin Lee <gb94@ewhain.net>
2024-07-21 22:41:00 +0900

    🐛 Fix: 카카오맵 api 오류 수정

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-//import { Map, MapMarker} from "react-kakao-maps-sdk";
+import { Map, MapMarker} from "react-kakao-maps-sdk";
 import MapFilter from '../../components/HomePage/MapFilter';
 import SideBar from '../../components/HomePage/SideBar';
 
@@ -8,7 +8,7 @@ const HomePage = () => {
             <SideBar />
             <MapFilter />
             <div style={{ position: "relative", width: "1920px", height: "1080px" }}>
-                {/* <Map
+                <Map
                     center={{ lat: 37.56011030387691, lng: 126.94585449321849 }}
                     style={{
                         position: "absolute",
@@ -17,7 +17,7 @@ const HomePage = () => {
                         width: "100%",
                         height: "100%",
                     }}
-                ></Map> */}
+                ></Map>
             </div>
         </div>
     )
