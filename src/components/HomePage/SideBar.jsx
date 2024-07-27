aba96e1d8627fd69ac1c9133a7181a19c4e49452
yoonjin-C <cyy000123@gmail.com>
2024-07-28 01:32:30 +0200

    ♻️ Refactor : 플레이리스트 라우팅 주소 수정

--- src/components/HomePage/SideBar.jsx
+++ src/components/HomePage/SideBar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import sidebar_logo from '../../assets/sidebar/sidebar_logo.svg';
 import sidebar_home from '../../assets/sidebar/sidebar_home.svg';
 import sidebar_search from '../../assets/sidebar/sidebar_search.svg';
@@ -11,35 +11,42 @@ import sidebar_mypage from '../../assets/sidebar/sidebar_mypage.svg';
 
 const SideBar = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const [activePath, setActivePath] = useState(location.pathname);
+
+    useEffect(() => {
+        setActivePath(location.pathname);
+    }, [location]);
 
     const onItemClick = (path) => {
+        setActivePath(path);
         navigate(path);
     };
 
     return (
         <SideBarContainer>
             <PinFeatures>
-                <StyledButton onClick={() => onItemClick('/home')} noBackground>
+                <StyledButton onClick={() => onItemClick('/home')} noBackground={activePath === '/home'}>
                     <HomeLogo src={sidebar_logo} alt="Home Logo" />
                 </StyledButton>
-                <StyledButton onClick={() => onItemClick('/home')}>
+                <StyledButton onClick={() => onItemClick('/home')} isActive={activePath === '/home'}>
                     <Home src={sidebar_home} alt="Home" />
                 </StyledButton>
-                <StyledButton onClick={() => onItemClick('/search')}>
+                <StyledButton onClick={() => onItemClick('/search')} isActive={activePath === '/search'}>
                     <Search src={sidebar_search} alt="Search" />
                 </StyledButton>
-                <StyledButton onClick={() => onItemClick('/create')}>
+                <StyledButton onClick={() => onItemClick('/create')} isActive={activePath === '/create'}>
                     <Create src={sidebar_create} alt="Create" />
                 </StyledButton>
-                <StyledButton onClick={() => onItemClick('/playlist')}>
+                <StyledButton onClick={() => onItemClick('/playlists')} isActive={activePath === '/playlist'}>
                     <Playlist src={sidebar_playlist} alt="Playlist" />
                 </StyledButton>
             </PinFeatures>
             <UserFeatures>
-                <StyledButton onClick={() => onItemClick('/users')}>
+                <StyledButton onClick={() => onItemClick('/users')} isActive={activePath === '/users'}>
                     <UserSearch src={sidebar_usersearch} alt="Search User" />
                 </StyledButton>
-                <StyledButton onClick={() => onItemClick('/mypage')}>
+                <StyledButton onClick={() => onItemClick('/mypage')} isActive={activePath === '/mypage'}>
                     <MyPage src={sidebar_mypage} alt="MyPage" />
                 </StyledButton>
             </UserFeatures>
@@ -55,7 +62,6 @@ const SideBarContainer = styled.div`
     justify-content: space-between;
     width: 80px;
     height: 100%;
-    /* border: 1px solid var(--gray, #BCBCBC); */
     background: var(--f8f8f8, #FCFCFC);
     z-index: 100; // 지도 위로 출력
 `;
@@ -81,31 +87,28 @@ const UserFeatures = styled.div`
 `;
 
 const StyledButton = styled.button`
-    background: none;
+    width: 60px;
+    height: 60px;
+    background: ${({ isActive, noBackground }) => (isActive && !noBackground ? 'var(--light_black, #232323)' : 'none')};
     border: none;
     padding: 0;
-    margin: 20px 0;
+    margin: 15px 0;
     cursor: pointer;
     display: flex;
     align-items: center;
     justify-content: center;
+    position: relative;
+    border-radius: ${({ isActive }) => (isActive ? '50%' : 'none')};
 
     &:focus {
         outline: none;
     }
 
-    &:focus::after {
-        content: '';
-        position: absolute;
-        width: 60px;
-        height: 60px;
-        background: ${({ noBackground }) => (noBackground ? 'none' : 'var(--light_black, #232323)')};
-        border-radius: 50%;
-        z-index: -1;
-    }
-
-    &:hover {
-    }
+    ${props => props.isActive && `
+        & img {
+            filter: brightness(0) invert(1);
+        }
+    `}
 `;
 
 const HomeLogo = styled.img`
