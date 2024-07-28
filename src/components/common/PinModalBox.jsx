2b4c44e1ba1cfb08d3806c393df8a75f2176816f
yoonjin-C <cyy000123@gmail.com>
2024-07-28 14:30:31 +0200

    💄 UI : 핀컴포넌트 더보기 팝업창 적용

--- src/components/common/PinModalBox.jsx
+++ src/components/common/PinModalBox.jsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import moreButton from "../../assets/images/MyPage/more-icon.svg";
 import SmallModal from "./Modal/SmallModal";
+import AddPlaylistModal from "./Modal/AddPlaylistModal";
+import { useNavigate } from "react-router-dom";
 
-const options = ["플레이리스트에 추가", "핀 수정", "핀 삭제"];
+const options = ["담기", "수정", "삭제"];
 
 const PinModalBox = ({ top, right, padding }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [clickedOption, setClickedOption] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isAddPlaylistModalOpen, setIsAddPlaylistModalOpen] = useState(false);
 
   const handlePopup = () => {
     setIsOpen(!isOpen);
@@ -17,16 +20,31 @@ const PinModalBox = ({ top, right, padding }) => {
   const handleOptionClick = option => {
     setClickedOption(option);
     setIsOpen(false);
-    handleModal();
+    if (option === "삭제") {
+      handleDeleteModal();
+    } else if (option === "담기") {
+      handleAddPlaylistModal();
+    } else if (option === "수정") {
+      goPinEditPage();
+    }
   };
 
-  const handleModal = () => {
-    setIsModalOpen(prevState => !prevState);
+  const handleDeleteModal = () => {
+    setIsDeleteModalOpen(prevState => !prevState);
   };
 
   const handleDeletePin = () => {
-    setIsModalOpen(false);
+    setIsDeleteModalOpen(false);
     window.location.reload();
+  };
+
+  const handleAddPlaylistModal = () => {
+    setIsAddPlaylistModalOpen(prevState => !prevState);
+  };
+
+  const navigate = useNavigate();
+  const goPinEditPage = () => {
+    navigate("/pin-edit");
   };
 
   return (
@@ -41,13 +59,17 @@ const PinModalBox = ({ top, right, padding }) => {
           ))}
         </MorePopup>
       )}
-      {isModalOpen && clickedOption === "핀 삭제" && (
+      {isDeleteModalOpen && clickedOption === "삭제" && (
         <SmallModal
           text="핀을 삭제할까요?"
-          onClose={handleModal}
+          onClose={handleDeleteModal}
           onDecide={handleDeletePin}
         />
       )}
+      {isAddPlaylistModalOpen && clickedOption === "담기" && (
+        <AddPlaylistModal setModalCommon={handleAddPlaylistModal} />
+      )}
+      {clickedOption === "수정" && { goPinEditPage }}
     </PinModal>
   );
 };
@@ -66,27 +88,26 @@ const MoreBtn = styled.img`
 
 const MorePopup = styled.div`
   display: flex;
-  /* width: 197px; */
-  padding: 18px;
+  padding: 12px 14px;
   flex-direction: column;
   align-items: flex-start;
-  gap: 12px;
+  gap: 6px;
   flex-shrink: 0;
   border-radius: 8px;
   border: 1px solid var(--gray02, #747474);
-  background: var(--f8f8f8, #fcfcfc);
-  z-index: 1000;
+  background: var(--offwhite_, #fcfcfc);
   position: absolute;
   top: ${props => props.positionTop};
   right: ${props => props.positionRight};
+  z-index: 1;
 `;
 
 const ListItem = styled.div`
   color: var(--light_black, #232323);
   font-family: Pretendard;
-  font-size: 20px;
+  font-size: 16px;
   font-style: normal;
-  font-weight: 500;
-  line-height: normal;
+  font-weight: 400;
+  line-height: 150%; /* 24px */
   cursor: pointer;
 `;
