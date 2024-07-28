0f7b2956852429bfc9e7ac9fd9baf227f6f6fa66
yoonjin-C <cyy000123@gmail.com>
2024-07-28 14:31:03 +0200

    ✨ Feat : 날짜 api 적용

--- src/components/PlaylistPage/Playlist.jsx
+++ src/components/PlaylistPage/Playlist.jsx
@@ -6,7 +6,13 @@ import BookmarkToggle from './BookmarkToggle';
 const Playlist = ({ playlist , onClick }) => {
   const { playlistName, creatorNickname, pinCount, updatedDate, imgPathList, bookmarkId, playlistId } = playlist;
   const [isHovered, setIsHovered] = useState(false);
-
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}.${month}.${day}`;
+  };
   return (
     <PlaylistContainer >
       <PlaylistBox>
@@ -31,7 +37,7 @@ const Playlist = ({ playlist , onClick }) => {
           <PinNum>{pinCount}</PinNum>
         </PinBox>
       </NameBox>
-      <UpdatedDate>최근 업데이트: {updatedDate}</UpdatedDate>
+      <UpdatedDate>최근 업데이트: {formatDate(updatedDate)}</UpdatedDate>
     </PlaylistContainer>
   );
 };
