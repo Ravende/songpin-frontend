3158566ef504feb3b0c0970ba254e9f5d712b49e
yoonjin-C <cyy000123@gmail.com>
2024-08-01 11:58:12 +0200

    ✨ Feat : 플레이리스트 편집 API 연결

--- src/pages/PlaylistPage/PlaylistEditPage.jsx
+++ src/pages/PlaylistPage/PlaylistEditPage.jsx
@@ -1,30 +1,33 @@
-import React, { useState,useEffect } from 'react';
-import styled from 'styled-components';
-import { useNavigate,useParams } from 'react-router-dom';
-import backArrow from '../../assets/images/UsersPage/arrow_back_ios.svg';
-import checked from '../../assets/images/PlaylistPage/checkbox_checked.svg';
-import unchecked from '../../assets/images/PlaylistPage/checkbox_unchecked.svg';
-import PinComponent from '../../components/PlaylistPage/PinComponent';
-import SideSection from '../../components/common/SideSection';
-import PublicToggle from '../../components/common/PublicToggle';
-import { getPlaylistDetail,updatePlaylist } from '../../services/api/stats';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useNavigate, useParams } from "react-router-dom";
+import backArrow from "../../assets/images/UsersPage/arrow_back_ios.svg";
+import checked from "../../assets/images/PlaylistPage/checkbox_checked.svg";
+import unchecked from "../../assets/images/PlaylistPage/checkbox_unchecked.svg";
+import PinComponent from "../../components/PlaylistPage/PinComponent";
+import SideSection from "../../components/common/SideSection";
+import PublicToggle from "../../components/common/PublicToggle";
+import SmallModal from "../../components/common/Modal/SmallModal";
+import { getPlaylistDetail, editPlaylist } from "../../services/api/stats";
 
 const PlaylistEditPage = () => {
   const { playlistId } = useParams();
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
   const [playlistData, setPlaylistData] = useState(null);
   const [isChecked, setIsChecked] = useState(false);
   const [isPublic, setIsPublic] = useState(true); // 추가: 공개 여부 상태
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [pinList, setPinList] = useState([]); // 추가: 핀 리스트 상태
 
   useEffect(() => {
     const fetchPlaylistDetail = async () => {
       try {
         const data = await getPlaylistDetail(playlistId);
+        console.log("Fetched Playlist Data:", data); // 콘솔에 데이터 출력
         setPlaylistData(data);
         setInputValue(data.playlistName);
-        setIsPublic(data.visibility === 'PUBLIC');
+        setIsPublic(data.visibility === "PUBLIC");
         setPinList(data.pinList.map(pin => ({ ...pin, isSelected: false }))); // 핀 데이터에 isSelected 추가
       } catch (error) {
         console.error("Error fetching playlist detail:", error);
@@ -33,11 +36,16 @@ const PlaylistEditPage = () => {
 
     fetchPlaylistDetail();
   }, [playlistId]);
-  const handleChange = (event) => {
+
+  const handleChange = event => {
     const { value } = event.target;
     if (value.length <= 40) {
       setInputValue(value);
     }
+  };
+
+  const handlePopup = () => {
+    setIsOpen(!isOpen);
   };
 
   const handleBackClick = () => {
@@ -49,25 +57,41 @@ const PlaylistEditPage = () => {
     setPinList(pinList.map(pin => ({ ...pin, isSelected: newIsChecked }))); // 모든 핀의 isSelected 상태 업데이트
   };
 
-  const handleToggleClick = () => {
-    setIsPublic((prev) => !prev);
-  };
-  const handleUpdatePlaylist = async () => {
+  const handleEditPlaylist = async () => {
     try {
-      const selectedPins = pinList.filter(pin => pin.isSelected);
-      await updatePlaylist(playlistId, inputValue, isPublic ? 'PUBLIC' : 'PRIVATE', pinList.length, pinList);
+      const selectedPins = pinList
+        .filter(pin => pin.isSelected)
+        .map(pin => ({
+          playlistPinId: pin.playlistPinId,
+          pinIndex: pin.pinIndex,
+        }));
+
+      await editPlaylist(
+        playlistId,
+        inputValue,
+        isPublic ? "PUBLIC" : "PRIVATE",
+        selectedPins.length, // 선택된 핀의 개수
+        selectedPins, // 수정된 핀 리스트
+      );
+
       navigate(-1);
     } catch (error) {
       console.error("Error updating playlist:", error);
     }
   };
 
-  const handlePinSelect = (id) => {
+  const handlePinSelect = id => {
     setPinList(prev =>
-      prev.map(pin => (pin.playlistPinId === id ? { ...pin, isSelected: !pin.isSelected } : pin))
+      prev.map(pin =>
+        pin.playlistPinId === id
+          ? { ...pin, isSelected: !pin.isSelected }
+          : pin,
+      ),
     );
   };
-
+  const handleDeleteSelectedPins = () => {
+    setPinList(prev => prev.filter(pin => !pin.isSelected));
+  };
 
   return (
     <SideSection>
@@ -75,37 +99,58 @@ const PlaylistEditPage = () => {
         <ContentBox>
           <BackBtn src={backArrow} onClick={handleBackClick} />
           <MainText>플레이리스트 편집</MainText>
-          {/* 나중에 이부분에 모달창 추가  */}
-          <BtnText isDisabled={!inputValue} onClick={handleUpdatePlaylist}>완료</BtnText>
+          <BtnText isDisabled={!inputValue} onClick={handlePopup}>
+            완료
+          </BtnText>
+          {isOpen && (
+            <SmallModal
+              text="편집한 내용을 저장할까요?"
+              onClose={handlePopup}
+              onDecide={handleEditPlaylist}
+            />
+          )}
         </ContentBox>
         <ContentBox>
           <BodyText>플레이리스트 이름</BodyText>
           <Edit>
-              <EditBox>              
-                <EditText type="text" value={inputValue} onChange={handleChange} /> 
+            <EditBox>
+              <EditText
+                type="text"
+                value={inputValue}
+                onChange={handleChange}
+              />
               <AlarmMessage>{inputValue.length}/40</AlarmMessage>
-              </EditBox>
-              <Line />
-            </Edit>
+            </EditBox>
+            <Line />
+          </Edit>
         </ContentBox>
         <ContentBox>
           <BodyText>공개 여부</BodyText>
-          <PublicToggle setVisibility={setIsPublic}/>
+          <PublicToggle isPublic={isPublic} setIsPublic={setIsPublic} />
         </ContentBox>
         <ContentBox>
           <SelectBox>
-            <TotalIcon src={isChecked ? checked : unchecked} alt="전체선택 버튼" onClick={handleCheckClicked} />
+            <TotalIcon
+              src={isChecked ? checked : unchecked}
+              alt="전체선택 버튼"
+              onClick={handleCheckClicked}
+            />
             <SelectText>전체선택</SelectText>
           </SelectBox>
-          {/* 나중에 이부분에 삭제 모달창 추가 및 api 연결  */}
-          <BtnText>삭제</BtnText>
+          <BtnText onClick={handleDeleteSelectedPins}>삭제</BtnText>
         </ContentBox>
         <PinContainer>
-        {pinList.map(pin => (
-            <PinComponent key={pin.playlistPinId} pin={pin} selectable={true} buttonVisible={false} onSelect={handlePinSelect}/>
+          {pinList.map(pin => (
+            <PinComponent
+              key={pin.playlistPinId}
+              pin={pin}
+              selectable={true}
+              buttonVisible={false}
+              onSelect={handlePinSelect}
+            />
           ))}
         </PinContainer>
-        </EditContainer>
+      </EditContainer>
     </SideSection>
   );
 };
@@ -113,9 +158,9 @@ const PlaylistEditPage = () => {
 export default PlaylistEditPage;
 
 const EditContainer = styled.div`
-padding:34px;
-padding-top:40px`;
-
+  padding: 34px;
+  padding-top: 40px;
+`;
 
 const ContentBox = styled.div`
   display: flex;
@@ -144,7 +189,8 @@ const MainText = styled.div`
 `;
 
 const BtnText = styled.div`
-    color: ${({ isDisabled }) => (isDisabled ? 'var(--gray, #BCBCBC)' : 'var(--light_black, #232323)')};
+  color: ${({ isDisabled }) =>
+    isDisabled ? "var(--gray, #BCBCBC)" : "var(--light_black, #232323)"};
   font-family: Pretendard;
   font-size: 20px;
   font-style: normal;
@@ -220,31 +266,8 @@ const AlarmMessage = styled.div`
 `;
 
 const EditBox = styled.div`
-display:flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-`
-
-const ToggleBox = styled.div`
-display:flex;
-flex-direction: row;
-align-items: center;
-`
-
-const ToggleText = styled.div`
-color: var(--gray02, #747474);
-font-family: Pretendard;
-font-size: 16px;
-font-style: normal;
-font-weight: 400;
-line-height: 150%; /* 24px */
-padding-right:16px;
-`
-
-const ToggleBtn = styled.img`
-width: 60px;
-height: 30px;
- transform: ${({ isPublic }) => (isPublic ? 'rotate(0deg)' : 'rotate(180deg)')};
- cursor:pointer;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
